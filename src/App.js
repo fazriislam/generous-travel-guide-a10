6455bc9b724ed8f31a7843d3e086347276e6335d
Fazri Isalm <fazriislam.work@gmail.com>
2022-04-17 21:36:45 +0600

    Banner Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Blogs from './Pages/Blogs/Blogs';
+import Banner from './Pages/Home/Banner/Banner';
+import Home from './Pages/Home/Home';
+import Header from './Pages/Shared/Header/Header';
+import NotFound from './Pages/Shared/NotFound/NotFound';
+import Tours from './Pages/Tours/Tours';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/tours' element={<Tours/>}/>
+        <Route path='/blogs' element={<Blogs/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='text-align-center'>
+            <h2>404</h2>
+            <p>Not Found</p>
+        </div>
+    );
+};
+
+export default NotFound;
