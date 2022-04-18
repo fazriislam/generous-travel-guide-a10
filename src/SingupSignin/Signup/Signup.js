9d8dd48e0bc014e54fe173554ba16516d5b92c09
Fazri Isalm <fazriislam.work@gmail.com>
2022-04-18 16:15:00 +0600

    Sign up with Email Verification

--- src/SingupSignin/Signup/Signup.js
+++ src/SingupSignin/Signup/Signup.js
@@ -1,29 +1,84 @@
-import React from 'react';
+import { async } from '@firebase/util';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useUpdateProfile } from 'react-firebase-hooks/auth';
+import { useSendEmailVerification } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Signup = () => {
+    const [name, setName] = useState([]);
+    const [email, setEmail] = useState([]);
+    const [password, setPassword] = useState([]);
+    const [confirmPassword, setConfirmPassword] = useState([]);
+    // const [error, setError] = useState([]);
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+    const [updateProfile, updating, profileError] = useUpdateProfile(auth);
+    const [sendEmailVerification, sending, verificationError] = useSendEmailVerification(auth);
+
+
+    // --------------------------------Event Functions
+
+    const handleNameBlur = e => {
+        setName(e.target.value);
+        console.log(name);
+    }
+
+    const handleEmailBlur = e => {
+        setEmail(e.target.value);
+    }
+
+    const handlePAsswordBlur = e => {
+        setPassword(e.target.value);
+    }
+
+    const handleConfirmPAsswordBlur = e => {
+        setConfirmPassword(e.target.value);
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        createUserWithEmailAndPassword(email, password);
+        updateProfile({ name: name });
+    }
+
+
+
     return (
         <div className='w-50 mt-5 mx-auto'>
             <h2 className='text-center text-primary'>Generous Travel Guide</h2>
-            <Form className='border p-3'>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form onSubmit={handleSubmit} className='border p-3'>
+                <Form.Group className="mb-3">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" placeholder="Your Name" />
+                    <Form.Control onBlur={handleNameBlur} type="text" placeholder="Your Name" />
                 </Form.Group>
-                
+
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control onBlur={handlePAsswordBlur} type="password" placeholder="Password" />
                 </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control onBlur={handleConfirmPAsswordBlur} type="password" placeholder="Confirm Password" />
+                </Form.Group>
+
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
-                <Button variant="primary" type="submit">
+                <Button onClick={handleSubmit} variant="primary" type="submit">
                     Sign up
                 </Button>
             </Form>
