bdda17aebc2b5b1d191aa71871419ec09bc6d1d8
Fazri Isalm <fazriislam.work@gmail.com>
2022-04-18 16:51:55 +0600

    Complete Sign in

--- src/SingupSignin/Signin/Signin.js
+++ src/SingupSignin/Signin/Signin.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Signin = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const navigate = useNavigate();
+    const [signInWithGoogle] = useSignInWithGoogle(auth);
+
+
+    // ----------------------Event Function
+
+    const handleEmailBlur = e => {
+        setEmail(e.target.value);
+    }
+
+    const handlePasswordBlur = e => {
+        setPassword(e.target.value);
+    }
+
+    // const handleGoogleSignIn=e=>{
+    //     signInWithGoogle();
+    // }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        signInWithEmailAndPassword(email, password);
+        navigate('/home');
+    }
+
     return (
-        <div>
-            <h2>Sign in</h2>
+        <div className='w-50 mt-5 mx-auto'>
+            <Form onSubmit={handleSubmit} className='border p-3'>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button onClick={handleSubmit} variant="primary" type="submit">
+                    Sign in
+                </Button>
+            </Form>
+            <hr />
+            <div className='mt-2 text-center'>
+                <Button onClick={() => signInWithGoogle()}>Sign in with Google</Button>
+            </div>
         </div>
     );
 };
--- src/SingupSignin/Signup/Signup.js
+++ src/SingupSignin/Signup/Signup.js
@@ -6,12 +6,13 @@ import { useUpdateProfile } from 'react-firebase-hooks/auth';
 import { useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import auth from '../../firebase.init';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
-    const [name, setName] = useState([]);
-    const [email, setEmail] = useState([]);
-    const [password, setPassword] = useState([]);
-    const [confirmPassword, setConfirmPassword] = useState([]);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     // const [error, setError] = useState([]);
 
     const [
@@ -22,6 +23,7 @@ const Signup = () => {
     ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
     const [updateProfile, updating, profileError] = useUpdateProfile(auth);
     const [sendEmailVerification, sending, verificationError] = useSendEmailVerification(auth);
+    const navigate = useNavigate();
     const googleProvider = new GoogleAuthProvider();
 
 
@@ -53,6 +55,7 @@ const Signup = () => {
                 const errorMessage = error.message;
                 const emailError = error.email;
             })
+            navigate('/home')
     }
 
     const handleSubmit = e => {
@@ -60,6 +63,7 @@ const Signup = () => {
 
         createUserWithEmailAndPassword(email, password);
         updateProfile({ name: name });
+        navigate('/home')
     }
 
 
@@ -76,7 +80,7 @@ const Signup = () => {
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
+                    <Form.Label>Email</Form.Label>
                     <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
                 </Form.Group>
 
